2a1a1f6d23bd807cb84e0787cc1c94ee3b3c86cd
Farusha <fariko2002@mail.ru>
2025-05-04 14:01:12 +0600

    added type module + changed server.js file

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
-// server.js
-const jsonServer = require("json-server");
-const path = require("path");
+import jsonServer from "json-server";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const server = jsonServer.create();
 const router = jsonServer.router(path.join(__dirname, "json-server/db.json"));
